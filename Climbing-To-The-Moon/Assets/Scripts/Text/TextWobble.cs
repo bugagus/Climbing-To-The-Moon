8e33409b1833f9b4afb4362a7633502024f7fc35
Rafael Expósito <r.exposito.2018@gmail.com>
2024-04-21 16:14:56 +0200

    Fonts and zoom in out text

--- Climbing-To-The-Moon/Assets/Scripts/Text/TextWobble.cs
+++ Climbing-To-The-Moon/Assets/Scripts/Text/TextWobble.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 
 public class TextWobble : MonoBehaviour
 {
@@ -62,6 +63,23 @@ public class TextWobble : MonoBehaviour
         {
             int wordIndex = wordIndexes[w];
             Word word = _words[w];
+            float scaleFactor = Mathf.Sin(Time.time * word.speed) * word.amplitude + 1;
+
+            // Encuentra el centro de la palabra
+            Vector3 wordCenter = Vector3.zero;
+            for (int i = 0; i < wordLengths[w]; i++)
+            {
+                int charIndex = wordIndex + i;
+                TMP_CharacterInfo c = textMesh.textInfo.characterInfo[charIndex];
+                int index = c.vertexIndex;
+
+                // Suma los vértices de los caracteres de la palabra
+                wordCenter += vertices[index] + vertices[index + 1] + vertices[index + 2] + vertices[index + 3];
+            }
+            // Calcula el centro promediando los vértices de la palabra
+            wordCenter /= wordLengths[w] * 4;
+
+
             Vector3 offset = Wobble(Time.time + w, word.xOffset, word.yOffset);
 
             for (int i = 0; i < wordLengths[w]; i++)
@@ -77,6 +95,12 @@ public class TextWobble : MonoBehaviour
                     gradient = word.color;
                 else
                     gradient = new Gradient();
+
+                // Escala los vértices del carácter alrededor del centro de la palabra
+                vertices[index] = wordCenter + (vertices[index] - wordCenter) * scaleFactor;
+                vertices[index + 1] = wordCenter + (vertices[index + 1] - wordCenter) * scaleFactor;
+                vertices[index + 2] = wordCenter + (vertices[index + 2] - wordCenter) * scaleFactor;
+                vertices[index + 3] = wordCenter + (vertices[index + 3] - wordCenter) * scaleFactor;
 
 
                 colors[index] = gradient.Evaluate(Mathf.Repeat(Time.time + vertices[index].x * 0.001f, 1f));
