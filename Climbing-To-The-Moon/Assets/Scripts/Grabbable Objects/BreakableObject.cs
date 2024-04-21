e02465bb539efe06348666a994475ea12aa37d50
Rafael Expósito <r.exposito.2018@gmail.com>
2024-04-21 21:58:22 +0200

    Destructive object

---
+++ Climbing-To-The-Moon/Assets/Scripts/Grabbable Objects/BreakableObject.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public abstract class BreakableObject : MonoBehaviour
+{
+    [Header("HealthObject")]
+    [SerializeField, Range(0.0f, 10.0f)] protected float _health;
+    [SerializeField, Range(1f, 10.0f)] protected float _decreaseHealth;
+    [SerializeField, Range(1f, 10.0f)] protected float _increaseHealth;
+
+    protected bool _isDestroyed;
+
+    protected virtual void Awake()
+    {
+        _isDestroyed = false;
+    }
+
+    protected abstract void Update();
+
+    protected abstract void DestroyObject();
+
+}
