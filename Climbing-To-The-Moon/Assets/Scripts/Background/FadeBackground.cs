f9e5cc5b6060d28cc19ea9b613e4e49a7adacef2
SergioMC2800 <158738287+SergioMC2800@users.noreply.github.com>
2024-04-25 00:25:44 +0200

    Sonidos, skin objeto rompible(?

--- Climbing-To-The-Moon/Assets/Scripts/Background/FadeBackground.cs
+++ Climbing-To-The-Moon/Assets/Scripts/Background/FadeBackground.cs
@@ -17,7 +17,6 @@ public class FadeBackground : MonoBehaviour
 
     private void Update()
     {
-        Debug.Log(_beetle.transform.position.y);
         if ((_beetle.transform.position.y >= _bg1Height && _beetle.transform.position.y < _bg2Height) || (_beetle.transform.position.y < _bg1Height))
         {
             animatorBg1.SetBool("Background", true);
